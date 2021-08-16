1b97426134b77ffe64751a88e23126bbbb0f830e
mmontes0794 <mmontes_0794@hotmail.com>
2021-08-16 13:53:31 -0500

    Se añade programa main.py al repositorio github_test_Michael_Montes

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+print("      Hoy adivinare en que año naciste")
+print()
+print()
+fecha = int(input("¿En que año estamos?   "))
+print()
+edad=int(input("¿Cuantos años tienes o vas a cumpir este año?   "))
+print()
+nacimiento = fecha - edad
+
+print ("Ya lo tengo naciste en el año   " + str(nacimiento))
+print()
+print("Versión beta de programa")
